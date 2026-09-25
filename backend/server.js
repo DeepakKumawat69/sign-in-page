1cb888256c999d50809655592213e19a3c9b2709
DeepakKumawat69 <deepakumawat659@gmail.com>
2026-09-25 19:46:47 +0530

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
 // Routes
 app.use('/api/auth', authRoutes);
 
